27012429ea8af8d1dca3a9f77eced9c9095aaaaf
yaochengjian <915248459@qq.com>
2020-06-16 13:37:31 +0800

    创建三个页面

---
+++ src/views/Labels.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>labels</div>
+</template>
+
+<script>
+export default {
+  name: "Labels"
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>statistics</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss"></style>
